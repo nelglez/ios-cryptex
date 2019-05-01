2bf7acc889467aa77cd99731693269a94a3f386c
Nelson Gonzalez <nelglezfl@gmail.com>
2019-05-01 15:30:48 -0400

    show a crypt using the delegate and datasource of pickerView

--- Cryptex/Cryptex/View Controller/CryptexViewController.swift
+++ Cryptex/Cryptex/View Controller/CryptexViewController.swift
@@ -14,13 +14,61 @@ class CryptexViewController: UIViewController {
     @IBOutlet weak var pickerView: UIPickerView!
     
     
+    var cryptexController = CryptexController()
+    
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        updateViews()
     }
 
+    private func updateViews() {
+    
+        guard let cryptexHint = cryptexController.currentCryptex else { return }
+        
+        hintLabel.text = cryptexHint.hint
+        
+        pickerView.reloadAllComponents()
+    
+    }
+    
+    
     @IBAction func unlockButtonPressed(_ sender: UIButton) {
+        
+        
     }
     
 }
 
+extension CryptexViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // For the number of components, think about how you can figure out how many characters are in the `currentCryptex`'s password.
+        
+        return cryptexController.currentCryptex?.password.count ?? 0
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // For the number of rows, we want to show as many rows as there are letters.
+        return letters.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // For the title of each row, we want to show the letter that corresponds to the row. i.e. row 0 should show "A", row 1 should show "B", etc.
+        
+        return letters[row]
+    }
+    
+    
+}
